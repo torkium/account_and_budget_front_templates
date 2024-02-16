23e1130527851d536533fc96c7fbb8e383f9869a
torkium <julien.taront@gmail.com>
2024-02-16 01:54:55 +0100

    add main template with side menu

--- js/navigation.js
+++ js/navigation.js
@@ -1,5 +1,13 @@
 document.getElementById("menuButton").addEventListener("click", function () {
-  var menu = document.getElementById("mobileNavItems");
+  let menu = document.getElementById("mobileNavItems");
+  if (menu.classList.contains("hidden")) {
+    menu.classList.remove("hidden");
+  } else {
+    menu.classList.add("hidden");
+  }
+});
+document.getElementById("sideMenuButton").addEventListener("click", function () {
+  let menu = document.getElementById("sidebar");
   if (menu.classList.contains("hidden")) {
     menu.classList.remove("hidden");
   } else {
